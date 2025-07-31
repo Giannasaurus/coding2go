3c79c86112adb15d180cbff7d38ae772c076e2e0
giannasaurus <gianzcantor@gmail.com>
2025-07-31 19:10:13 +0800

    add more stuff

--- scroll-anim/script.js
+++ scroll-anim/script.js
@@ -1,14 +1,33 @@
-const create = (tag) => document.createElement(tag);
+const create = (element) => document.createElement(element);
+const queryAll = (element) => document.querySelectorAll(element);
 
 function renderItems() {
     const wrapper = create('div');
-    let items = [];
+    const items = [];
 
-    for (let i = 0; i < 100; i++) {
+    for (let i = 1; i <= 25; i++) {
+        // from here
         const item = create('p');
         item.innerText = `Item ${i}`;
+        item.classList.add('item', 'item-style');
         items.push(item);
     }
+
+    wrapper.append(...items);
+    document.querySelector('body').appendChild(wrapper);
 }
 
 renderItems();
+
+const observer = new IntersectionObserver((items) => {
+    items.forEach((item) => {
+        if (item.isIntersecting) {
+            // to here
+            console.log(item.target);
+            item.classList.add('show');
+        }
+    })
+}, {});
+
+const itemElements = queryAll('.item');
+itemElements.forEach((item) => observer.observe(item));
