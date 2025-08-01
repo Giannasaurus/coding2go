b254b2df711eed3fba5a82ceab58754f119f8224
giannasaurus <gianzcantor@gmail.com>
2025-08-02 07:12:49 +0800

    finally fixed it ..

--- scroll-anim/script.js
+++ scroll-anim/script.js
@@ -6,7 +6,6 @@ function renderItems() {
     const items = [];
 
     for (let i = 1; i <= 25; i++) {
-        // from here
         const item = create('p');
         item.innerText = `Item ${i}`;
         item.classList.add('item', 'item-style');
@@ -22,12 +21,13 @@ renderItems();
 const observer = new IntersectionObserver((items) => {
     items.forEach((item) => {
         if (item.isIntersecting) {
-            // to here
             console.log(item.target);
-            item.classList.add('show');
+            item.target.classList.add('show');
+        } else {
+            item.target.classList.remove('show');
         }
     })
 }, {});
 
 const itemElements = queryAll('.item');
-itemElements.forEach((item) => observer.observe(item));
+itemElements.forEach((element) => observer.observe(element));
